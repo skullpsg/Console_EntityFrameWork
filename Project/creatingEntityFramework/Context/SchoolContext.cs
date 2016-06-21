fa45f9b83ad929f3f66c12952e1c0e983ec34760
skullpsg <vishnusankar.sakthivel@gmail.com>
2016-06-21 18:22:31 +0530

    console_EntityFramework

---
+++ Project/creatingEntityFramework/Context/SchoolContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace creatingEntityFramework.Context
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("name=SchoolDBConnectionString")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Standard> Standards { get; set; }
+
+    }
+}
---
+++ Project/creatingEntityFramework/Program.cs
@@ -0,0 +1,42 @@
+﻿using creatingEntityFramework.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace creatingEntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                //this will create the database
+                Student stud = new Student() { StudentName = "New Student" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+
+                //this will delete the database
+                try
+                {                   
+                    if (ctx.Database.Exists())
+                        ctx.Database.Delete();
+                    else {
+                        Console.Write("database Not Available");
+                        Console.ReadKey();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Write("database NOt deleted");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
